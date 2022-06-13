f18b3d54db77588d020777be6b9167c12a89d267
kriffer <antonio.kravets@gmail.com>
2022-06-13 22:22:05 +0300

    procfile and prod build of ui

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fullstackopen-p3
 exercises set for part 3 of fullstackopen course
+
+Online demo: https://fathomless-springs-80400.herokuapp.com/
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms  :post-data`))
 morgan.token('post-data', function getPost(req) {
@@ -103,6 +103,8 @@ const generateId = () => {
     : 0
   return maxId + 1
 }
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
